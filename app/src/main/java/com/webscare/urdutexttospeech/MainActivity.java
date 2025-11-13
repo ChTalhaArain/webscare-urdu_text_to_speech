a2c701bfd9301d6e84fd9c46442e68a93b9ed4d8
ChTalhaArain <ch.talha.arain@outlook.com>
2025-11-13 16:55:24 +0500

    Light mode color resources + basic navigation

--- app/src/main/java/com/webscare/urdutexttospeech/MainActivity.java
+++ app/src/main/java/com/webscare/urdutexttospeech/MainActivity.java
@@ -7,12 +7,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.webscare.urdutexttospeech.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding UI;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        UI = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(UI.getRoot());
+
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +35,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        BottomNavigationView bottomNav = UI.bottomNav;
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment == null) {
+            System.err.println("NavHostFragment is null");
+            return;
+        }
+
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        UI = null;
     }
 }
